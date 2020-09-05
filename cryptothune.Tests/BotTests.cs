41059ee7aff0a613d53703116169ffbc8d709c4e
Christophe Desplanches <kcdesplanches@gmail.com>
2020-09-05 19:05:23 -0400

    Align .net core version almongst projects.

--- cryptothune.Tests/BotTests.cs
+++ cryptothune.Tests/BotTests.cs
@@ -20,42 +20,29 @@ namespace Cryptothune.Tests
             var portfolio = new PortfolioFake(exchange);
             portfolio.Money = 500.0;    // Put 500 EUR as a gift.
             var bot = new BotThune(portfolio);
-            bot.Sim(new Funiol());
+            bot.Sim(new Funiol(), "XTZEUR");
         }
 
         [Fact]
         public void TestDryRun()
         {
             /*
-            var wallet = new Wallet("XTZEUR");
-            wallet.Deposit(500.0);
-
-            var bot = new BotThune<ExchangeKraken>();
-            bot.DryRun(new Funiol(), wallet);
+            var exchange = new ExchangeKraken();
+            var portfolio = new Portfolio(exchange);
+            var bot = new BotThune(portfolio);
+            bot.Run(new Funiol(), "XTZEUR" );
             */
         }
 
         [Fact]
         public void TestRun()
         {
+            /*
             var exchange = new ExchangeKraken();
             var portfolio = new Portfolio(exchange);
             var bot = new BotThune(portfolio);
             bot.Run(new Funiol(), "XTZEUR" );
-
-            /*
-            var wallet = new Wallet("XTZEUR");
-            wallet.Deposit(500.0);
-
-            var bot = new BotThune<ExchangeKraken>();
-            var strategy = new Funiol();
-            
-            bot.Run(strategy, wallet);
             */
-
-            //bot.Run("BTCEUR");
-
-            //bot.Run("XRPEUR");
         }
     }
 }
